bdafffd420286616a92aaa6886233ecd7a70e565
James Baldwin <jimwbaldwin@gmail.com>
2018-08-02 00:11:27 +1000

    Initial version of import.py

---
+++ import.py
@@ -0,0 +1,26 @@
+import csv
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+def main():
+    f = open("books.csv")
+    insert_count = 0
+    reader = csv.reader(f)
+    for isbn_text, title_text, author_name, published_year in reader:
+      print("loop")
+      if insert_count != 0: #skip the header
+        db.execute("INSERT INTO books (isbn_text, title_text, author_name, published_year) VALUES (:isbn_text, :title_text, :author_name, :published_year)",
+          {"isbn_text": isbn_text, "title_text": title_text, "author_name": author_name, "published_year": published_year})
+        print(insert_count)
+      insert_count += 1
+    db.commit()
+    print(f"Rows inserted: {insert_count}")
+
+if __name__ == "__main__":
+    main()
+
